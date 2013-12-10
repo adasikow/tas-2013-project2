5a2e0d021dd5638291fa58b48400150527b9c0ba
adasikow <adasikow@gmail.com>
2013-12-10 22:38:40 +0100

    Update products/url
    Usunąłem zbędne komentarze

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,5 @@
 from django.conf.urls import *
 
-#category_patterns = patterns('products.views',
-#    (r'^$',     'list_products_from_category'),
-#    (r'^top$', 'list_top_products_from_category'),
-#    (r'^worst$', 'list_worst_products_from_category'),
-#)
-
 urlpatterns = patterns('products.views',
     (r'^$',     'list_products'),
     (r'^(\d+)$', 'product_page'),
@@ -31,7 +25,4 @@ urlpatterns = patterns('products.views',
         'sport|clothes|beauty|health|'
         'gifts|jewelry|kids|media|food|'
         'hobby|building)/worst$', 'list_worst_products'),
-    #(r'^category/(?P<category_name>\w+)^$', 'list_products_from_category'),
-    #(r'^category/(?P<category_name>\w+)/top$', 'list_top_products_from_category'),
-    #(r'^category/(?P<category_name>\w+)/worst$', 'list_worst_products_from_category'),
 )
