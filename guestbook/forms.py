7d50dbf62fe1c30b6cc6adddd963a2d9c41bb8ba
adasikow <adasikow@gmail.com>
2013-12-12 02:35:30 +0100

    Update

--- guestbook/forms.py
+++
@@ -1,8 +0,0 @@
-﻿# Django forms model, "django-guestbook/guestbook/forms.py"
-from django import forms
-from guestbook.models import Greeting
-
-class CreateGreetingForm(forms.ModelForm):
-    class Meta:
-        model = Greeting
-        exclude = ('author', 'date') # same as "fields = ('content',)"
--- guestbook/urls.py
+++
@@ -1,7 +0,0 @@
-# Django URL configuration, "django-guestbook/guestbook/urls.py"
-from django.conf.urls import *
-
-urlpatterns = patterns('guestbook.views',
-    (r'^$',     'list_greetings'),
-    (r'^sign$', 'create_greeting'),
-)
