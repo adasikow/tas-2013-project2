b8cb4331f62d6f5d7e0d2e7abe05c787f8f1192e
adasikow <adasikow@gmail.com>
2014-01-23 02:16:30 +0100

    Projekt_3
    Dodany test dla produktu, dodane pliki niezbędne do działania tastypie
    (niestety obecnie nie działa)

--- products/tests.py
+++ products/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from products.models import *
+from products.views import calculate_rating
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ProductReviewTest(TestCase):
+    def setUp(self):
+        product = Product(name = "A10-7850", description = "Kaveri APU", producer = "AMD")
+        user = User(username = "jan")
+        user.set_password("kowalski")
+        product.save()
+        user.save()
+        ProductReview.objects.create(product = product, author = user, content = "ok")
+        
+        
+    def test_calculate_rating(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Sprawdza, czy domyslna ocena w recenzji produktu to 5.0 oraz czy poprawnie dziala obliczanie oceny produktu
         """
-        self.assertEqual(1 + 1, 2)
+        product = Product.objects.get(name = "A10-7850")
+        self.assertEqual(calculate_rating(product.id), 4.0)
+        
