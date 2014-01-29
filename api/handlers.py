4add552b9b9b3a4523c96d5c2dc20c474a07d80f
adasikow <adasikow@gmail.com>
2014-01-29 22:35:08 +0100

    REST
    Dodawanie produktów oraz pobieranie informacji o produktach zrobione,
    właściwie to chyba wszystko działa. Linki:
    localhost:8000/api/products - pobiera wszystkie produkty
    localhost:8000/api/product/<nazwa_produktu> - pobiera produkt o podanej
    nazwie
    
    localhost:8000/api/product/<nazwa_produktu>/<opis>/<producent>/<kategoria>
    - dodaje produkt do bazy i zwraca xml z informacjami o tym produkcie
    (nie jest sprawdzana poprawność danych, np. kategoria!)
    localhost:8000/api/product, post data:
    "name=<nazwa>&description=<opis>&producer=<producent>&category=<kategoria>"
    również dodaje produkt do bazy, tutaj jednak kategoria musi być
    poprawna!
    w przypadku curl zapytania GET wywołuje się poprzez:
    curl url
    zapytania POST:
    curl -d "<post data>" url
    W przypadku dodawania produktu za pomocą linku string <post data> musi
    być pusty, ponieważ nie pobieramy informacji z post data. W drugim
    przypadku string post data musi być wypełniony tak, jak wyżej podałem

--- api/handlers.py
+++ api/handlers.py
@@ -1,5 +1,6 @@
 from piston.handler import BaseHandler
-from products.models import Product
+from products.models import *
+from products.forms import *
 
 class ProductHandler(BaseHandler):
     allowed_methods = ('GET', 'POST',)
@@ -13,10 +14,13 @@ class ProductHandler(BaseHandler):
     
 
     def create(self, request, product_name = None, product_desc = None, product_producer = None, product_category = None):
-    
+        
+        product = None
         if(product_name and product_desc and product_producer): #przekazywanie danych w linku
             product = Product(name = product_name, description = product_desc, producer = product_producer, category = product_category)
         else: # przekazywanie danych POST
-            product = Product(name = request.POST['name'], description = request.POST['description'], producer = request.POST['producer'], category = request.POST['category'])
+            form = AddProductForm(request.POST)
+            product = form.save(commit = False)
+            #product = Product(name = request.POST['name'], description = request.POST['description'], producer = request.POST['producer'], category = request.POST['category'])
         product.save()
         return product
