c663e7cf882fc878e7062cf8541958801aeca959
adasikow <adasikow@gmail.com>
2014-01-29 21:37:18 +0100

    REST
    Dodawanie obiektów. Polecenie curl:
    curl -d ""
    "http://127.0.0.1:8000/api/product/<nazwa_produktu>/<opis_produktu>/<producent>/<kategoria>?format=xml"
    Link zwraca plik xml z informacjami o dodanym produkcie

--- api/handlers.py
+++ api/handlers.py
@@ -2,11 +2,21 @@ from piston.handler import BaseHandler
 from products.models import Product
 
 class ProductHandler(BaseHandler):
-    allowed_methods = ('GET',)
+    allowed_methods = ('GET', 'POST',)
     model = Product
    
-    def read(self, request, product_name=None):
+    def read(self, request, product_name):
         if(product_name):
             return Product.objects.get(name = product_name)
         else:
             return Product.objects.all()
+    
+
+    def create(self, request, product_name = None, product_desc = None, product_producer = None, product_category = None):
+    
+        if(product_name and product_desc and product_producer): #przekazywanie danych w linku
+            product = Product(name = product_name, description = product_desc, producer = product_producer, category = product_category)
+        else: # przekazywanie danych POST
+            product = Product(name = request.name, description = request.description, producer = request.producer, category = request.category)
+        product.save()
+        return product
--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls.defaults import *
 from piston.resource import Resource
 from api.handlers import *
 
-product_handler = Resource(ProductHandler)
+class CsrfExemptResource( Resource ):
+    def __init__( self, handler, authentication = None ):
+        super( CsrfExemptResource, self ).__init__( handler, authentication )
+        self.csrf_exempt = getattr( self.handler, 'csrf_exempt', True )
+
+product_handler = CsrfExemptResource( ProductHandler )
 
 urlpatterns = patterns('',
-   (r'^product$', product_handler),
-   (r'^products$', product_handler),
+    (r'^product/(?P<product_name>.*)/(?P<product_desc>.*)/(?P<product_producer>.*)/(?P<product_category>.*)$', product_handler),
+    (r'^products$', product_handler),
 )
