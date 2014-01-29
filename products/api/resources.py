a45bdc3337967f9176beef55163d50212edc1443
adasikow <adasikow@gmail.com>
2014-01-30 00:11:26 +0100

    REST
    Usunięte tastypie i inne śmieci

--- products/api/resources.py
+++
@@ -1,7 +0,0 @@
-from tastypie.resources import ModelResource
-from products.models import *
-
-class ProductResource(ModelResource):
-    class Meta:
-        queryset = Product.objects.all()
-        allowed_methods = ['get']
