f10af4dae8fb6630771ad3bab6854241bb110d2d
adasikow <adasikow@gmail.com>
2014-01-30 09:11:28 +0100

    REST
    zmieniony skrypt, wczyszczona baza produktów

--- bot.bat
+++ bot.bat
@@ -1,17 +1,5 @@
 curl localhost:8000/api/products > stan_poczatkowy.xml
-curl -d "name=NORTON AntiVirus 2013&description=Antywirus&producer=Norton&category=computer" localhost:8000/api/product
-curl -d "name=ASUS X550CC&description=i3 8GB 500GB GT720M 2GB&producer=Asus&category=computer" localhost:8000/api/product
-curl -d "name=Canon MG3250&description=Tusz do drukarek&producer=Canon&category=computer" localhost:8000/api/product
-curl -d "name=NAVROAD Nexo GO IPS&description=16GB Quad Core WWA&producer=NAVROAD&category=computer" localhost:8000/api/product
-curl -d "name=VERBATIM 1TB&description=DYSK Zewnêtrzny VERBATIM 1TB 2,5'' USB 3.0 FV&producer=Verbatim&category=computer" localhost:8000/api/product
-curl -d "name=ENERMAX UCTB12&description=SILENCE 120mm 11dB GW&producer=ENERMAX&category=computer" localhost:8000/api/product
-curl -d "name=SWITCH CISCO 3550&description=Prze³¹cznik L3 - 48 portów FE + 2 Porty 1GB GBIC do obsadzenia&producer=CISCO&category=computer" localhost:8000/api/product
-curl -d "name=Samsung SL-M2675FN&description=Urz¹dzenie 4w1&producer=Samsung&category=computer" localhost:8000/api/product
-curl -d "name=WD 120GB&description=NOWY WD 120GB 5400RPM 2.5 SATA GW&producer=Dell&category=computer" localhost:8000/api/product
-curl -d "name=BATERIA DELL J1KND&description=nowa bateria do notebooków Dell Inspiron Vostro 5200 mAh 11,1V o wysokiej pojemnoœci i wydajnoœci&producer=Dell&category=computer" localhost:8000/api/product
-curl -d "name=Creative Sound Blaster Tactic3D Rage USB&description=S³uchawki&producer=Creative&category=computer" localhost:8000/api/product
-curl -d "name=Genius SP-S110&description=Genius SP-S110 czarne&producer=Genius&category=computer" localhost:8000/api/product
-curl -d "name=HP LCD Speaker Bar&description=G³oœniki&producer=HP&category=computer" localhost:8000/api/product
-curl -d "name=ZTE MF60&description=Modem&producer=ZTE&category=computer" localhost:8000/api/product
-curl -d "name=LevelOne WBR-6003&description=Router&producer=LevelOne&category=computer" localhost:8000/api/product
+for /l %%x in (1, 1, 100) do (
+   curl -d "name=test%%x&description=test%%x&producer=test%%x&category=computer" localhost:8000/api/product
+)
 curl localhost:8000/api/products > stan_koncowy.xml
